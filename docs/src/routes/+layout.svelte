cd79fdaa54b816b372102df1bd4816958917fbb5
homindolentrahar <rahardiyanekowidiatmoko@gmail.com>
2024-12-08 23:26:59 +0700

    docs: Creating initial commit of docs web

---
+++ docs/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "../app.css";
+</script>
+
+<main class="bg-slate-50 dark:bg-slate-950 h-screen">
+  <slot />
+</main>
---
+++ docs/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="flex flex-col justify-center items-center px-8 py-12 h-full">
+  <div class=" flex flex-col items-center gap-4">
+    <h1
+      class="text-3xl md:text-4xl font-semibold text-slate-950 dark:text-white"
+    >
+      Indcana Docs
+    </h1>
+    <a
+      href="https://github.com/homindolentrahar/indcana"
+      target="_blank"
+      class="bg-blue-500 hover:bg-blue-500/90 dark:bg-blue-600 dark:hover:bg-blue-600/90 px-4 py-2 text-white font-semibold rounded-md transition-all"
+      >See Package</a
+    >
+  </div>
+</div>
