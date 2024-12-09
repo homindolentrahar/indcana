04ebe536711efb5cec586d7722daa4ec0f3b2e49
homindolentrahar <rahardiyanekowidiatmoko@gmail.com>
2024-12-09 22:33:05 +0700

    docs: Installing shadcn on Sveltekit

--- docs/src/routes/+page.svelte
+++ docs/src/routes/+page.svelte
@@ -1,15 +1,16 @@
+<script>
+  import Button from "$lib/components/ui/button/button.svelte";
+</script>
+
 <div class="flex flex-col justify-center items-center px-8 py-12 h-full">
-  <div class=" flex flex-col items-center gap-4">
+  <div class=" flex flex-col items-center gap-6">
     <h1
       class="text-3xl md:text-4xl font-semibold text-slate-950 dark:text-white"
     >
       Indcana Docs
     </h1>
-    <a
-      href="https://github.com/homindolentrahar/indcana"
-      target="_blank"
-      class="bg-blue-500 hover:bg-blue-500/90 dark:bg-blue-600 dark:hover:bg-blue-600/90 px-4 py-2 text-white font-semibold rounded-md transition-all"
-      >See Package</a
+    <Button href="https://github.com/homindolentrahar/indcana" target="_blank"
+      >View Package</Button
     >
   </div>
 </div>
